d58f47a8b76fcbba4e37259280459c5b410889aa
sheldar <kewin.polok@gmail.com>
2020-08-26 20:23:10 +0200

    SRM and EBC calculation

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 
 import calculatePlatoToOG from '../utils/calculatePlatoToOG';
@@ -19,27 +20,45 @@ const StyledPaper = styled(Paper)`
 
 interface Malt {
   ebc: number;
-  volume: number;
+  weight: number;
 }
 
 const Home: NextPage = () => {
   const [plato, setPlato] = useState(12);
   const [og, setOg] = useState(calculatePlatoToOG(plato));
-  const [malts, setMalts] = useState<Malt[]>([]);
+  const [malts, setMalts] = useState<Malt[]>([
+    {
+      ebc: 3.6,
+      weight: 5,
+    },
+  ]);
 
-  const totalEbc = malts.reduce((ebc, malt) => {
+  const [batchVolume] = useState(20);
+
+  const mcu = malts.reduce((mcu, malt) => {
     const srm = malt.ebc * 0.508;
     const lovibond = (srm + 0.76) / 1.3546;
 
-    return ebc;
+    console.log('SRM', srm);
+    console.log('°Lovibond', lovibond);
+
+    const weightInLbs = malt.weight * 2.20462;
+    console.log('lbs', weightInLbs);
+    const batchVolumeInGaloons = batchVolume * 0.264172;
+    console.log('gal', batchVolumeInGaloons);
+
+    return mcu + (lovibond * weightInLbs) / batchVolumeInGaloons;
   }, 0);
+
+  const srmMorey = 1.4922 * Math.pow(mcu, 0.6859);
+  const ebcMorey = srmMorey * 1.97;
 
   const addMalt = () => {
     setMalts([
       ...malts,
       {
         ebc: 0,
-        volume: 0,
+        weight: 0,
       },
     ]);
   };
@@ -53,18 +72,16 @@ const Home: NextPage = () => {
       <Container>
         <StyledPaper>
           <Grid container spacing={4}>
-            <Grid container item spacing={1} xs={12}>
+            <Grid container item spacing={1}>
               <Grid item>
-                <form noValidate autoComplete="off">
-                  <TextField
-                    label="°Plato"
-                    onChange={(event) => {
-                      setPlato(Number(event.target.value));
-                    }}
-                    type="number"
-                    value={plato}
-                  />
-                </form>
+                <TextField
+                  label="°Plato"
+                  onChange={(event) => {
+                    setPlato(Number(event.target.value));
+                  }}
+                  type="number"
+                  value={plato}
+                />
               </Grid>
               <Grid item>
                 <TextField
@@ -75,19 +92,22 @@ const Home: NextPage = () => {
                 />
               </Grid>
             </Grid>
-            <Grid container item spacing={1} xs={12}>
+            <Grid container item spacing={2}>
               {malts.map((malt, index) => (
-                <Grid container item key={index} spacing={1} xs={12}>
+                <Grid container item key={index} spacing={1}>
                   <Grid item>
-                    <form noValidate autoComplete="off">
-                      <TextField label="EBC" type="number" value={malt.ebc} />
-                    </form>
+                    <TextField label="EBC" type="number" value={malt.ebc} />
                   </Grid>
                   <Grid item>
                     <TextField
-                      label="Volume"
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment position="end">kg</InputAdornment>
+                        ),
+                      }}
+                      label="Weight"
                       type="number"
-                      value={malt.volume}
+                      value={malt.weight}
                     />
                   </Grid>
                 </Grid>
@@ -97,12 +117,25 @@ const Home: NextPage = () => {
                   Add malt
                 </Button>
               </Grid>
+            </Grid>
+            <Grid container item spacing={1}>
+              <Grid item>
+                <TextField disabled label="MCU" type="number" value={mcu} />
+              </Grid>
               <Grid item>
                 <TextField
                   disabled
-                  label="EBC"
+                  label="SRM Morey"
                   type="number"
-                  value={totalEbc}
+                  value={srmMorey}
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  disabled
+                  label="EBC Morey"
+                  type="number"
+                  value={ebcMorey}
                 />
               </Grid>
             </Grid>
