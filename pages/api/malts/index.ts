29a65c88b70445edcf05e73f40760675348a7559
sheldar <kewin.polok@gmail.com>
2020-09-27 12:58:02 +0200

    More predefined malts

--- pages/api/malts/index.ts
+++ pages/api/malts/index.ts
@@ -8,9 +8,34 @@ export interface PredefinedMalt {
 
 const MALTS: PredefinedMalt[] = [
   {
+    ebc: 900,
+    extract: 67,
+    name: 'Czekoladowy ciemny',
+  },
+  {
+    ebc: 200,
+    extract: 75,
+    name: 'Karmelowy 200',
+  },
+  {
+    ebc: 22,
+    extract: 78,
+    name: 'Monachijski ciemny',
+  },
+  {
     ebc: 6.25,
     extract: 81.6,
     name: 'Pale Ale',
+  },
+  {
+    ebc: 3.65,
+    extract: 80,
+    name: 'Pilzneński',
+  },
+  {
+    ebc: 70,
+    extract: 75,
+    name: 'Red Ale',
   },
 ];
 
