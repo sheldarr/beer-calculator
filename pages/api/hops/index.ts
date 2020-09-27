fa8d37d7c7966745c8a8e7a2c7df4ed587bfde34
sheldar <kewin.polok@gmail.com>
2020-09-27 13:05:37 +0200

    More predefined hops

--- pages/api/hops/index.ts
+++ pages/api/hops/index.ts
@@ -7,8 +7,44 @@ export interface PredefinedHop {
 
 const HOPS: PredefinedHop[] = [
   {
+    alphaAcids: 5.8,
+    name: 'Cascade',
+  },
+  {
+    alphaAcids: 5.2,
+    name: 'Hallertauer Tradition',
+  },
+  {
+    alphaAcids: 9.4,
+    name: 'Iunga',
+  },
+  {
+    alphaAcids: 5,
+    name: 'Lomik',
+  },
+  {
+    alphaAcids: 6,
+    name: 'Lubelski',
+  },
+  {
     alphaAcids: 6.7,
     name: 'Marynka',
+  },
+  {
+    alphaAcids: 7.1,
+    name: 'Oktawia',
+  },
+  {
+    alphaAcids: 3.5,
+    name: 'Saaz',
+  },
+  {
+    alphaAcids: 6.2,
+    name: 'Sybilla',
+  },
+  {
+    alphaAcids: 14.4,
+    name: 'Zeus',
   },
 ];
 
